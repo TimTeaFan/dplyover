9ab93f879481eab69b7d0abf2b20807d5401c4f6
TimTeaFan <github@e-niversity.com>
2020-07-10 23:43:22 +0200

    added package .R file

---
+++ R/dplyover.R
@@ -0,0 +1,7 @@
+#' @description
+#' To learn more about dplyover, start with the vignette:
+#' `browseVignettes(package = "dplyover")`
+#' @useDynLib dplyover, .registration = TRUE
+#' @keywords internal
+#' @importFrom rlang %||%
+"_PACKAGE"
