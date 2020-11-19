f2be78ff5c3e1e636a2356a1c27f096d265c8a9e
TimTeaFan <github@e-niversity.com>
2020-11-20 00:23:08 +0100

    Initialized `across2()`.

---
+++ R/across2.R
@@ -0,0 +1,160 @@
+#' Loop two inputs simultaneaously over one or several functions in 'dplyr'
+#'
+#' @description
+#' ...
+#'
+#' @inheritParams over
+#'
+#' @param .x,.y An atomic vector (expect 'raw' and 'complex') to apply functions to.
+#'   Instead of a vector a <[`selection helper`][selection_helpers]> or anything else
+#'   that is coercible to an atomic vector can be used. Note that `over()` must only
+#'   be used to create 'new' columns and will throw an error if `.x` contains
+#'   existing column names. To transform existing columns use [dplyr::across()].
+#'
+#' @param ... Additional arguments for the function calls in `.fns`.
+#'
+#' @param .names A glue specification that describes how to name the output
+#'   columns. This can use `{vec}` to stand for the selected vector element, and
+#'   `{fn}` to stand for the name of the function being applied. The default
+#'   (`NULL`) is equivalent to `"{vec}"` for the single function case and
+#'   `"{vec}_{fn}"` for the case where a list is used for `.fns`.
+#'
+#' @returns
+#' A tibble with one column for each element in `.x` and each function in `.fns`;.
+#'
+#' @section Examples:
+#'
+#' ```{r, child = "man/rmd/setup.Rmd"}
+#' ```
+#'
+#' `over()` can only be used inside `dplyr::mutate` or `dplyr::summarise`.
+#' It has two main use cases. They differ in how the elements in `.x`
+#' are used. Let's first attach `dplyr`:
+#'
+#' ```{r, comment = "#>", collapse = TRUE}
+#' library(dplyr)
+#'
+#' # For better printing
+#' iris <- as_tibble(iris)
+#' ```
+#'
+#' @export
+across2 <- function(.cols1, .cols2, .fns, ..., .names = NULL){
+
+  .data <- tryCatch({
+    dplyr::across()
+  }, error = function(e) {
+    rlang::abort("`across2()` must only be used inside dplyr verbs")
+  })
+
+  .cnames <- names(.data)
+
+  check_keep()
+
+  setup <- across2_setup({{.cols1}},
+                         {{.cols2}},
+                         fns = .fns,
+                         names = .names,
+                         cnames = .cnames,
+                         data = .data)
+
+  vars1 <- setup$vars1
+  vars2 <- setup$vars2
+
+  if (length(vars1) == 0L && length(vars2)) {
+    return(tibble::new_tibble(list(), nrow = 1L))
+  }
+
+  fns <- setup$fns
+  names <- setup$names
+
+  # if (any(names %in% .cnames)) {
+  #   dnames <- .cnames[.cnames %in% names]
+  #   names_l <- ifelse(length(dnames) > 3, 3, length(dnames))
+  #
+  #   rlang::abort(c("Problem with `across2()`.",
+  #                  i = "Output must not contain existing column names.",
+  #                  x = paste0("`over()` tried to create the following existing column names: ",
+  #                             paste0(paste0("'", dnames[seq_along(1:names_l)], "'"), collapse = ", "),
+  #                             ifelse(length(dnames) > 3, " etc. ", ".")),
+  #                  i = "If you want to transform existing columns try using `across()`.",
+  #                  i = "If you want to change to output names use the `.names` argument"))
+  #
+  # }
+
+  data1 <- dplyr::select(dplyr::cur_data(), dplyr::all_of(vars1))
+  data2 <- dplyr::select(dplyr::cur_data(), dplyr::all_of(vars2))
+
+  n_cols1 <- length(data1)
+  n_fns <- length(fns)
+  seq_n_cols1 <- seq_len(n_cols1)
+  seq_fns <- seq_len(n_fns)
+  k <- 1L
+  out <- vector("list", n_cols1 * n_fns)
+
+  for (i in seq_n_cols1) {
+    col1 <- data1[[i]]
+    col2 <- data2[[i]]
+    for (j in seq_fns) {
+      fn <- fns[[j]]
+      out[[k]] <- fn(col1, col2, ...)
+      k <- k + 1L
+    }
+  }
+  size <- vctrs::vec_size_common(!!!out)
+  out <- vctrs::vec_recycle_common(!!!out, .size = size)
+  names(out) <- names
+  tibble::new_tibble(out, nrow = size)
+}
+
+
+across2_setup <- function(cols1, cols2, fns, names, cnames, data) {
+
+  cols1 <- rlang::enquo(cols1)
+  cols2 <- rlang::enquo(cols2)
+  group_vars <- names(dplyr::cur_group())
+  vars1 <- tidyselect::eval_select(rlang::expr(!!cols1), data)
+  vars2 <- tidyselect::eval_select(rlang::expr(!!cols2), data)
+  vars1 <- names(vars1)
+  vars2 <- names(vars2)
+
+  # check later
+  if (length(vars1) != length(vars2)) {
+    rlang::abort(c("Problem with `across2()` input `.cols1` and `.cols2`.",
+                   i = "Input `.cols1` and `.cols2` must use the same number of columns.",
+                   x = paste0(length(vars1), "columns are selected in `.cols1`, " , ,
+                              ", while ", length(vars2), " columns are selected in `.cols2`.")))
+  }
+
+  if (is.function(fns) || rlang::is_formula(fns)) {
+    names <- names %||% "{col1}_{col2}"
+    fns <- list(`1` = fns)
+  } else {
+    names <- names %||% "{col1}_{col2}_{fn}"
+  }
+
+  if (!is.list(fns)) {
+    rlang::abort(c("Problem with `over()` input `.fns`.",
+                   i = "Input `.fns` must be a function or a list of functions"))
+  }
+
+  fns <- purrr::map(fns, rlang::as_function)
+
+  if (is.null(names(fns))) {
+    names_fns <- seq_along(fns)
+  } else {
+    names_fns <- names(fns)
+    empties <- which(names_fns == "")
+    if (length(empties)) {
+      names_fns[empties] <- empties
+    }
+  }
+
+  names <- vctrs::vec_as_names(glue::glue(names,
+                                          col1 = rep(vars1, each = length(fns)),
+                                          col2 = rep(vars2, each = length(fns)),
+                                          fn = rep(names_fns, length(cols1))),
+                               repair = "check_unique")
+  value <- list(vars1 = vars1, vars2 = vars2, fns = fns, names = names)
+  value
+}
