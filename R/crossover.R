e62e3245594c74a3be0addad42fd4112314f63e0
TimTeaFan <github@e-niversity.com>
2020-11-22 23:25:41 +0100

    rewrote `crossover` including `each` and `crossXover`.

--- R/crossover.R
+++ R/crossover.R
@@ -82,6 +82,143 @@ crossover <- function(.cols, .x, .fns, ..., .names = NULL){
 
   }
 
+  if (setup$each) {
+    data <- dplyr::select(dplyr::cur_data(), dplyr::all_of(unique(vars)))
+    data_ls <- as.list(data)[vars]
+    data <- tibble::new_tibble(data_ls)
+  } else {
+    data <- dplyr::select(dplyr::cur_data(), dplyr::all_of(vars))
+  }
+
+  n_cols <- length(data)
+  n_fns <- length(fns)
+  seq_n_cols <- seq_len(n_cols)
+  seq_fns <- seq_len(n_fns)
+
+  k <- 1L
+  out <- vector("list", n_cols * n_fns)
+
+  for (i in seq_n_cols) {
+    col <- data[[i]]
+    xi <- x[[i]]
+    for (j in seq_fns) {
+      fn <- fns[[j]]
+      out[[k]] <- fn(col, xi, ...)
+      k <- k + 1L
+    }
+  }
+
+  size <- vctrs::vec_size_common(!!!out)
+  out <- vctrs::vec_recycle_common(!!!out, .size = size)
+  names(out) <- names
+  tibble::new_tibble(out, nrow = size)
+}
+
+
+crossover_setup <- function(cols, x1, fns, names, cnames, data, each = FALSE) {
+
+  cols <- rlang::enquo(cols)
+  vars <- tidyselect::eval_select(rlang::expr(!!cols), data)
+  vars <- names(vars)
+
+  if (is.function(x1) || rlang::is_formula(x1)) {
+  # call_x <- rlang::quo(x1)
+  name_x <- rlang::call_name(x1)
+  env_nm_x <- rlang::env_name(environment(rlang::call_fn(x1)))
+  x1 <- rlang::as_function(x1)
+  x1 <- purrr::map(dplyr::select(data, !! cols), x1)
+  }
+
+  if (name_x == "each" && env_nm_x == "namespace:dplyover") {
+  vars <- unlist(purrr::imap(x1, ~ rep(.y, length(.x))))
+  x1 <- unlist(x1, recursive = FALSE)
+  if (!is.list(x1)) x1 <- unname(x1)
+  each <- TRUE
+  } else if (length(vars) != length(x1)) {
+    rlang::abort(c("Problem with `crossover()` input `.cols` and `.x`.",
+                   i = "The number of `.cols` must be equal to the number of elements in  `.x`.",
+                   x = paste0(length(vars), " columns are selected in `.cols`, ",
+                              ", while `.x` contains ", length(x1), " elements.")))
+  }
+
+  if (is.function(fns) || rlang::is_formula(fns)) {
+    names <- names %||% "{col}_{x}"
+    fns <- list(`1` = fns)
+  } else {
+    names <- names %||% "{col}_{x}_{fn}"
+  }
+
+  if (!is.list(fns)) {
+    rlang::abort(c("Problem with `over()` input `.fns`.",
+                   i = "Input `.fns` must be a function or a list of functions"))
+  }
+
+  fns <- purrr::map(fns, rlang::as_function)
+
+  if (is.null(names(fns))) {
+    names_fns <- seq_along(fns)
+  } else {
+    names_fns <- names(fns)
+    empties <- which(names_fns == "")
+    if (length(empties)) {
+      names_fns[empties] <- empties
+    }
+  }
+
+  names <- vctrs::vec_as_names(glue::glue(names,
+                                          col = rep(vars, each = length(fns)),
+                                          x   = rep(names(x1) %||% x1, length(fns)),
+                                          fn  = rep(names_fns, length(vars))),
+                               repair = "check_unique")
+  value <- list(vars = vars, x = x1, fns = fns, names = names, each = each)
+  value
+}
+#
+
+
+crossXover <- function(.cols, .x, .fns, ..., .names = NULL){
+
+  .data <- tryCatch({
+    dplyr::across()
+  }, error = function(e) {
+    rlang::abort("`crossover()` must only be used inside dplyr verbs")
+  })
+
+  .cnames <- names(.data)
+
+  check_keep()
+
+  setup <- crossXover_setup({{.cols}},
+                           .x,
+                           fns = .fns,
+                           names = .names,
+                           cnames = .cnames,
+                           data = .data)
+
+  vars <- setup$vars
+  x <- setup$x
+
+  if (length(vars) == 0L) {
+    return(new_tibble(list(), nrow = 1L))
+  }
+
+  fns <- setup$fns
+  names <- setup$names
+
+  if (any(names %in% .cnames)) {
+    dnames <- .cnames[.cnames %in% names]
+    names_l <- ifelse(length(dnames) > 3, 3, length(dnames))
+
+    rlang::abort(c("Problem with `crossover()`.",
+                   i = "Output must not contain existing column names.",
+                   x = paste0("`crossover()` tried to create the following existing column names: ",
+                              paste0(paste0("'", dnames[seq_along(1:names_l)], "'"), collapse = ", "),
+                              ifelse(length(dnames) > 3, " etc. ", ".")),
+                   i = "If you want to transform existing columns try using `across()`.",
+                   i = "If you want to change the output names use the `.names` argument"))
+
+  }
+
   data <- dplyr::select(dplyr::cur_data(), dplyr::all_of(vars))
 
   n_cols <- length(data)
@@ -113,7 +250,7 @@ crossover <- function(.cols, .x, .fns, ..., .names = NULL){
 }
 
 
-crossover_setup <- function(cols, x1, fns, names, cnames, data) {
+crossXover_setup <- function(cols, x1, fns, names, cnames, data) {
 
   cols <- rlang::enquo(cols)
   vars <- tidyselect::eval_select(rlang::expr(!!cols), data)
@@ -150,4 +287,12 @@ crossover_setup <- function(cols, x1, fns, names, cnames, data) {
                                repair = "check_unique")
   value <- list(vars = vars, x = x1, fns = fns, names = names)
   value
+}
+
+
+
+# helper ----
+
+each <- function(fn) {
+  eval(fn)
 }
