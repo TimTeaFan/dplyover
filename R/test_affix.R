83f74841ed21dcdab10241dab0b0579578ea0ad2
TimTeaFan <github@e-niversity.com>
2021-01-08 00:36:46 +0100

    fix: test_ / get_affix functions to: - bare column names in test_affix were not working - get_affix was producing wrong results

--- R/test_affix.R
+++ R/test_affix.R
@@ -1,19 +1,26 @@
-show_prefix <- function(.data, .cols1, .cols2) {
-  show_affix(data = .data,
-             cols1 = .cols1,
-             cols2 = .cols2,
+test_prefix <- function(.data, .xcols, .ycols) {
+
+  .xcols <- rlang::enexpr(.xcols)
+  .ycols <- rlang::enexpr(.ycols)
+
+  test_affix(data = .data,
+             xcols = .xcols,
+             ycols = .ycols,
              type = "prefix")
 }
 
-show_suffix <- function(.data, .cols1, .cols2) {
-  show_affix(data = .data,
-             cols1 = .cols1,
-             cols2 = .cols2,
+test_suffix <- function(.data, .xcols, .ycols) {
+
+  .xcols <- rlang::enexpr(.xcols)
+  .ycols <- rlang::enexpr(.ycols)
+
+  test_affix(data = .data,
+             xcols = .xcols,
+             ycols = .ycols,
              type = "suffix")
 }
 
-# test_affix()
-show_affix <- function(data, cols1, cols2, type = c("prefix", "suffix")) {
+test_affix <- function(data, xcols, ycols, type = c("prefix", "suffix")) {
 
   group_vars <- group_vars(data)
 
@@ -22,39 +29,38 @@ show_affix <- function(data, cols1, cols2, type = c("prefix", "suffix")) {
     data <- dplyr::select(data, -dplyr::all_of(group_vars))
   }
 
-  cols1 <- rlang::enquo(cols1)
-  cols2 <- rlang::enquo(cols2)
+  xvars <- tidyselect::eval_select(xcols, data)
+  yvars <- tidyselect::eval_select(ycols, data)
 
-  vars1 <- tidyselect::eval_select(rlang::expr(!!cols1), data)
-  vars2 <- tidyselect::eval_select(rlang::expr(!!cols2), data)
+  xvars <- names(xvars)
+  yvars <- names(yvars)
 
-  vars1 <- names(vars1)
-  vars2 <- names(vars2)
-
-  if (length(vars1) != length(vars2)) {
-    rlang::abort(c(paste0("Problem with `test_", type,"()` input `.cols1` and `.cols2`."),
-                   i = "Input `.cols1` and `.cols2` must use the same number of columns.",
-                   x = paste0(length(vars1), " columns are selected in `.cols1`, ",
-                              "while ", length(vars2), " columns are selected in `.cols2`.")))
+  if (length(xvars) != length(yvars)) {
+    rlang::abort(c(paste0("Problem with `test_", type,"()` input `.xcols` and `.ycols`."),
+                   i = "Input `.xcols` and `.ycols` must use the same number of columns.",
+                   x = paste0(length(xvars), " columns are selected in `.xcols`, ",
+                              "while ", length(yvars), " columns are selected in `.colsy`.")))
   }
 
-  var_nms <- purrr::flatten(purrr::map2(vars1, vars2, ~ list(c(.x, .y))))
+  var_nms <- purrr::flatten(purrr::map2(xvars, yvars, ~ list(c(.x, .y))))
   if (type == "prefix") {
-    res <- purrr::map_chr(var_nms, ~ get_affix(.x, "prefix"))
+    res <- purrr::map(var_nms, ~ get_affix(.x, "prefix"))
   } else {
-    res <- purrr::map_chr(var_nms, ~ get_affix(.x, "suffix"))
+    res <- purrr::map(var_nms, ~ get_affix(.x, "suffix"))
   }
 
-  inp_tbl <- tibble::tibble(.cols1 = vars1,
-                            .cols2 = vars2,
+  res <- unlist(purrr::modify_if(res, rlang::is_empty, ~ NA_character_))
+
+  inp_tbl <- tibble::tibble(.xcols = xvars,
+                            .ycols = yvars,
                             !! type := res)
+
   print(inp_tbl, n = 10)
   if (nrow(inp_tbl) > 10) {
     cat("Use `.Last.value %>% View()` to see to full list of variables.")
     invisible(inp_tbl)
   }
 }
-
 
 # helper function for across2_setup
 get_affix <- function(x, type = c("prefix", "suffix")) {
@@ -67,20 +73,23 @@ get_affix <- function(x, type = c("prefix", "suffix")) {
   x_ls <- purrr::transpose(strsplit(x, ""))
   x_ls_length <- purrr::map_dbl(purrr::map(x_ls, unique), length)
   x_rle <- rle(x_ls_length)
-  if (side == "right") {
+  if (side == "right" && x_rle$values[1] == 1) {
     res <- stringr::str_sub(x[[1]],
                             start = 1L,
                             end = x_rle$length[1])
 
-  } else {
-    res_start <- sum(x_rle$length[-length(x_rle$length)])
+  } else if (side == "left" && x_rle$values[length(x_rle$values)] == 1) {
+    res_start <- sum(x_rle$length[-length(x_rle$length)]) + 1
     res_length <- x_rle$length[length(x_rle$length)]
     res_end <- res_start + res_length
 
     res <- stringr::str_sub(x[[1]],
                             start = res_start,
                             end = res_end)
+  } else {
+    res <- NULL
   }
+
   res <- stringr::str_remove_all(res, "[:punct:]*$")
   res <- stringr::str_remove_all(res, "^[:punct:]*")
 
@@ -98,4 +107,4 @@ get_affix <- function(x, type = c("prefix", "suffix")) {
 # x <- c("Sepal.Length", "Sepal.Width")
 # x <- c("Length.Sepal", "Width.Sepal")
 # x <- c("Length.of.Sepal.here", "Length.no.Sepal.here")
-# get_affix(x, "prefix")
+# get_affix(x, "suffix")
