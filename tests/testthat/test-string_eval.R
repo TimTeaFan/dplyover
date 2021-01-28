f5a16160d574fc241ba9c30aea1f63ae50d66d18
TimTeaFan <github@e-niversity.com>
2021-01-28 23:38:51 +0100

    tests: added tests for string_eval

---
+++ tests/testthat/test-string_eval.R
@@ -0,0 +1,43 @@
+# string_eval  ------------------------------------------------------------------
+# string_eval examples of basic functionality from the example section
+library(dplyr)
+
+## examples
+test_that(".() works in over()", {
+
+  df_over <- iris %>%
+    mutate(over(c("Sepal", "Petal"),
+                ~ .("{.x}.Width") + .("{.x}.Length")
+    ))
+
+  df_expect <- iris %>%
+    mutate(
+      Sepal = Sepal.Width + Sepal.Length,
+      Petal = Petal.Width + Petal.Length
+    )
+
+  expect_equal(df_over, df_expect)
+
+})
+
+# add non-dplyover examples? (across, map)
+
+## compability
+
+test_that("data.table's .() still works", {
+
+  expect_error(local({
+    library(data.table)
+    mtcarsDT <- as.data.table(mtcars)
+    mtcarsDT[, .(mpg, hp)]
+    detach("package:data.table", unload = TRUE)
+  }), NA)
+
+})
+
+
+test_that("magrittrs . still works", {
+
+  expect_error(mtcars %>% nrow(.), NA)
+
+})
