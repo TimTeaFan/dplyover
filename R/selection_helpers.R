3ed0a04bfa242f3491ef86ce403f359ddec3e0de
TimTeaFan <github@e-niversity.com>
2021-01-28 23:38:06 +0100

    doc: added selection_helpers overview

---
+++ R/selection_helpers.R
@@ -0,0 +1,20 @@
+#' Selection helpers
+#'
+#' @description
+#'
+#' `dplyover` provides two kinds of selection helpers which are intended for use
+#' in all functions that accept a vector as argument (that is `over()` and
+#' `crossover()` as well as all their variants, see here for a full list of the
+#' [over-across function family][over_across_family]).
+#'
+#' Helpers which select **string parts** of the **column names** (of the underyling data):
+#'  - [cut_names()] removes a specified pattern.
+#'  - [extract_names()] extracts a specified pattern.
+#'
+#' Helpers which select **values** of a variable:
+#'  - [dist_values()] returns all distinct values.
+#'  - [seq_range()] returns the sequence between the `range()` of a variable.
+#'
+#' @name selection_helpers
+#' @aliases over_selection_helpers
+NULL
