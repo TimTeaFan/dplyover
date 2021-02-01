2d1c99ccb66ae96cb8003e5b73110d84540bf6c2
TimTeaFan <github@e-niversity.com>
2021-02-01 23:03:58 +0100

    setup: added imports := and na.omit

--- R/dplyover.R
+++ R/dplyover.R
@@ -2,4 +2,6 @@
 #' To learn more about dplyover, start with the vignette:
 #' `browseVignettes(package = "dplyover")`
 #' @importFrom rlang %||%
+#' @importFrom rlang :=
+#' @importFrom stats na.omit
 "_PACKAGE"
