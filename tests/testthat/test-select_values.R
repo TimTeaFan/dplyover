e537fdd8021d98b6d60e57e338ccdb82d2ca6d11
TimTeaFan <github@e-niversity.com>
2021-06-17 00:06:42 +0200

    test(unique_tidy): tests for `unique_tidy` were added, tests for `dist_values?` were removed.

--- tests/testthat/test-select_values.R
+++ tests/testthat/test-select_values.R
@@ -3,10 +3,10 @@
 library(dplyr)
 
 ## examples
-test_that("use case: dist_values() in over()", {
+test_that("use case: unique_tidy() in over()", {
 
   over_dist_val <- iris %>%
-    mutate(over(dist_values(Species),
+    mutate(over(unique_tidy(Species),
                 ~ if_else(Species == .x, 1, 0)
     ),
     .keep = "none")
@@ -21,22 +21,153 @@ test_that("use case: dist_values() in over()", {
 
 })
 
-test_that("dist_values() short examples", {
+test_that("unique_tidy() supports tidyselect syntax", {
 
-  expect_equal(dist_values(c(1:3, NA)), c(1:3))
+  over_dist_val <- mtcars %>%
+    mutate(over(unique_tidy(vs:am),
+                ~ if_else(mpg == .x, 1, 0)
+    ),
+    .keep = "none")
 
-  expect_equal(dist_values(c(1:3, NA), .sort = "desc"), c(3:1))
+  df_expect <- mtcars %>%
+    mutate(`0` = 0,
+           `1` = 0,
+           .keep = "none")
 
-  expect_equal(dist_values(c(3, 1, 2, NA), .sort = "none"), c(3, 1, 2))
+  expect_equal(over_dist_val, df_expect)
+
+})
+
+test_that("unique_tidy() short examples", {
+
+  expect_equal(unique_tidy(c(1:3, NA), sort = "asc"), c(1:3))
+
+  expect_equal(unique_tidy(c(1:3, NA), sort = "desc"), c(3:1))
+
+  expect_equal(unique_tidy(c(3, 1, 2, NA), sort = "none"), c(3, 1, 2))
 
   expect_equal(
     factor(c(1:3, NA)) %>%
     as.factor() %>%
-    dist_values() %>%
+    unique_tidy() %>%
     class(), "character")
 
 })
 
+test_that("unique_tidy() works with comma separated vectors and lists", {
+
+  expect_equal(
+    c("1, 2, 3",
+      "2, 4, 5, 6",
+      "4, 1, 7") %>%
+      unique_tidy(., sep = ", "),
+    as.character(1:7)
+  )
+
+  expect_equal(
+    list(a = c(1:4), b = (4:6), c(5:10)) %>%
+      unique_tidy(),
+    c(1:10))
+
+})
+
+
+test_that("unique_tidy() example with factors", {
+
+  fctrs <- factor(letters[1:3], levels = c("b", "a", "c"))
+
+  expect_equal(unique_tidy(fctrs), c("b", "a", "c"))
+
+  expect_equal(unique_tidy(fctrs, sort = "none"), c("b", "a", "c"))
+
+  expect_equal(unique_tidy(fctrs, sort = "asc"), letters[1:3])
+
+  expect_equal(unique_tidy(fctrs, sort = "desc"), letters[3:1])
+
+
+
+})
+
+test_that("unique_tidy() `grp_data` argument works as expected", {
+
+  # warn character
+  expect_warning({
+    iris %>%
+      mutate(Species = as.character(Species)) %>%
+      group_by(Species) %>%
+      mutate(over(unique_tidy(Species),
+                   ~ if_else(Species == .x, 1, 0)))
+    })
+
+  # warn factor
+  expect_warning({
+    iris %>%
+      group_by(Species) %>%
+      mutate(over(unique_tidy(Species),
+                  ~ if_else(Species == .x, 1, 0)))
+  })
+
+  # silent
+  expect_warning({
+    iris %>%
+      group_by(Species) %>%
+      mutate(over(unique_tidy(Species, grp_data = "silent"),
+                  ~ if_else(Species == .x, 1, 0)))
+  }, NA)
+
+  # ungroup
+  expect_equal(
+    {iris %>%
+      mutate(Species = as.character(Species)) %>%
+      group_by(Species) %>%
+      transmute(over(unique_tidy(Species, grp_data = "ungroup"),
+                     ~ .x)) %>%
+      ncol()},
+    4L)
+})
+
+test_that("unique_tidy() supports tidy select in dplyr without dplyover", {
+  expect_equal({
+    mtcars %>%
+      mutate(values = list(unique_tidy(am:gear))) %>%
+      slice(1) %>%
+      pull(values) %>%
+      unlist},
+    c(0, 1, 3, 4, 5)
+    )
+})
+
+
+test_that("unique_tidy() `na.rm and `grp_data` arguments work as expected", {
+
+  expect_equal({
+    csat %>%
+      transmute(over(unique_tidy(email_rating,
+                                 na.rm = FALSE),
+                     ~ if_else(same_value(email_rating, .x), 1, 0))) %>%
+      ncol()
+  }, 6L)
+
+  expect_equal({
+    csat %>%
+      mutate(email_rating = as.character(email_rating)) %>%
+      transmute(over(unique_tidy(email_rating,
+                                 na.rm = FALSE),
+                     ~ if_else(same_value(email_rating, .x), 1, 0))) %>%
+      ncol()
+  }, 6L)
+
+  expect_equal({
+    csat %>%
+      group_by(email_rating) %>%
+      transmute(over(unique_tidy(email_rating,
+                                 grp_data = "ungroup",
+                                 na.rm = FALSE),
+                     ~ if_else(same_value(email_rating, .x), 1, 0))) %>%
+      ncol()
+    }, 7L)
+
+})
 
 test_that("use case: seq_rang() in over()", {
 
@@ -73,38 +204,6 @@ test_that("seq_rang() on dates", {
 
 })
 
-
-test_that("dist_values() works with comma separated vectors and lists", {
-
-  expect_equal(
-    c("1, 2, 3",
-      "2, 4, 5, 6",
-      "4, 1, 7") %>%
-      dist_values(., .sep = ", "),
-    as.character(1:7)
-  )
-
-  expect_equal(
-    list(a = c(1:4), b = (4:6), c(5:10)) %>%
-      dist_values(),
-    c(1:10))
-
-})
-
-
-test_that("dist_values() example with factors", {
-
-  fctrs <- factor(letters[1:3], levels = c("b", "a", "c"))
-
-  expect_equal(dist_values(fctrs), c("b", "a", "c"))
-
-  expect_equal(dist_values(fctrs, .sort = "asc"), letters[1:3])
-
-  expect_equal(dist_values(fctrs, .sort = "desc"), letters[3:1])
-
-  expect_equal(dist_values(fctrs, .sort = "none"), letters[1:3])
-
-})
 
 # more tests
 
